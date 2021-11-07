f005e4bf115620f999be512e03388060367db011
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2021-11-07 23:05:19 +0700

    chore: ignore example images when publish the package

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import {
+  materialPalenight,
+  materialPalenightTheme,
+  materialPalenightHighlightStyle
+} from './material-palenight';
+
+import {
+  materialDarker,
+  materialDarkerTheme,
+  materialDarkerHighlightStyle
+} from './material-darker';
+
+export {
+  materialPalenight,
+  materialPalenightTheme,
+  materialPalenightHighlightStyle,
+  materialDarker,
+  materialDarkerTheme,
+  materialDarkerHighlightStyle
+};
